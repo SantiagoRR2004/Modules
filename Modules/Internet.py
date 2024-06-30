9aa27b82f9c4c031e86159221ebbc8dbece54c5d
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-06-30 13:04:52 +0200

    Internet module changes

--- Modules/Internet.py
+++ Modules/Internet.py
@@ -5,11 +5,18 @@ import os
 import random
 import time
 from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import sqlite3
+from Cryptodome.Cipher import AES
+from Cryptodome.Protocol.KDF import PBKDF2
+import secretstorage
+import sys
+
 
 def downloadImage(url, dest_file):
     if not os.path.isfile(dest_file):
@@ -25,12 +32,24 @@ def mangasee123UrlsXML(web):
     urls = [item.find('link').text for item in page.find_all('item')]
     return urls[::-1]
 
-def configureChrome():
+def configureChrome() -> webdriver.Chrome:
+    """
+    Configures a Chrome WebDriver instance
+    and tries to say it is not automated
+    
+    Returns:
+        webdriver.Chrome: A WebDriver instance
+    """
     # pip install --upgrade selenium
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
-    chrome_options.binary_location = "/opt/google/chrome/google-chrome"
-    return webdriver.Chrome(options=chrome_options)
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    # userDataDir = os.path.join(os.path.expanduser("~"), ".config", "google-chrome", "Default")
+    # chrome_options.add_argument(f"user-data-dir={userDataDir}")
+    service = Service(ChromeDriverManager().install())
+    return webdriver.Chrome(options=chrome_options, service=service)
 
 def clickButton(driver,name):
     wait = WebDriverWait(driver, 10)  # Wait up to 10 seconds for the button to be clickable
@@ -129,4 +148,107 @@ def mangasee123Downloader(manga,chapter,maxDownload,callerDirectory,skip=[],miss
 
     zipping.zipAndDelete(folder)
 
+
+def getCookies() -> list:
+    """
+    Returns a list with the cookies from the Chrome browser.
+
+    It returns a list of dictionaries. Each dictionary 
+    contains the columns of the cookies table as keys
+    and the values of the columns as values.
+    """
+
+    userHome = os.path.expanduser("~")
+    cookiesPath = os.path.join(
+        userHome, ".config", "google-chrome", "Default", "Cookies"
+    )
+
+    conn = sqlite3.connect(cookiesPath)
+    cursor = conn.cursor()
+
+    cursor.execute("PRAGMA table_info(cookies);")
+
+    columnNames = [column[1] for column in cursor.fetchall()]
+
+    cursor.execute(f"SELECT {", ".join(columnNames)} FROM cookies;")
+    unformattedData = cursor.fetchall()
+    conn.close()
+    data = []
+
+    for row in unformattedData:
+        data.append(dict(zip(columnNames, row)))
+
+    return data
+
+
+def clean(x: bytes) -> str:
+    """
+    Removes the padding from the decrypted value.
+
+    Args:
+        x (bytes): The decrypted value.
+
+    Returns:
+        str: The decrypted value without padding.
+    """
+    try:
+        return x[: -x[-1]].decode("utf8")
+    except UnicodeDecodeError as e:
+        print(f"UnicodeDecodeError: {e}")
+        # Optionally, handle the error as needed
+        # For example, you can ignore errors or replace them:
+        return x[: -x[-1]].decode("utf8", errors="replace")
+
+
+def decryptCookies(cookies: list) -> list:
+    """
+    Decrypts the cookies from the Chrome browser.
+    https://stackoverflow.com/questions/23153159/decrypting-chromium-cookies/23727331#23727331
+
+    Args:
+        cookies (list): A list containing the cookies (each a dict).
+                    The key is the name of the column and the value is the value of the column.
+
+    Returns:
+        dict: A list containing the decrypted cookies (each a dict).
+                    The encrypted value key is replaced by value
+    """
+    bus = secretstorage.dbus_init()
+    collection = secretstorage.get_default_collection(bus)
+    for item in collection.get_all_items():
+        if item.get_label() == "Chrome Safe Storage":
+            MY_PASS = item.get_secret()
+            break
+    else:
+        raise Exception("Chrome password not found!")
+
+    decryptedCookies = []
+
+    for cookie in cookies:
+        # Trim off the 'v10' that Chrome/ium prepends
+        encrypted_value = cookie["encrypted_value"][3:]
+
+        if encrypted_value:
+
+            # Default values used by both Chrome and Chromium in OSX and Linux
+            salt = b"saltysalt"
+            iv = b" " * 16
+            length = 16
+
+            # 1003 on Mac, 1 on Linux
+            iterations = 1
+            if sys.platform.startswith("linux"):
+                iterations = 1
+            elif sys.platform.startswith("darwin"):
+                iterations = 1003
+
+            key = PBKDF2(MY_PASS, salt, length, iterations)
+            cipher = AES.new(key, AES.MODE_CBC, IV=iv)
+
+            decrypted = cipher.decrypt(encrypted_value)
+            del cookie["encrypted_value"]
+            cookie["value"] = clean(decrypted)
+            decryptedCookies.append(cookie)
+
+    return decryptedCookies
 
