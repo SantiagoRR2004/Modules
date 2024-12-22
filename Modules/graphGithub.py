0bc6df1703fab01e6a23031c04524b8d874d9fae
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-12-22 13:10:33 +0100

    Added addContributors function

--- Modules/graphGithub.py
+++ Modules/graphGithub.py
@@ -44,3 +44,45 @@ def addRepositories(graph: nx.MultiDiGraph) -> nx.MultiDiGraph:
                 pass
 
     return graph
+
+
+def addContributors(graph: nx.MultiDiGraph) -> nx.MultiDiGraph:
+    """
+    Add the contributors for all the nodes that have
+    the type "Repository" in the graph. It adds the contributors
+    as their url.
+
+    When we have found all the contributors of a repository, we mark it
+    in the attribute "searchData" with the key "githubContributors"
+    so we don't have to search it again.
+
+    Args:
+        - graph (nx.MultiDiGraph): The graph to be modified.
+
+    Returns:
+        nx.MultiDiGraph: The graph with the contributors.
+    """
+    nodes, attributeList = zip(*graph.nodes(data=True))
+
+    for node, attributes in zip(nodes, attributeList):
+        if "Repository" in attributes["type"]:
+            if not attributes.get("searchData", False) or not attributes[
+                "searchData"
+            ].get("githubContributors", False):
+                contributors = github.getContributors(node)
+
+                for c in contributors:
+                    if c not in graph.nodes():
+                        graph.add_node(c, type=("GitHub", "User"), color=github.COLOR)
+                        graph.add_edge(c, node)
+
+                if attributes.get("searchData", False):
+                    graph.nodes[node]["searchData"]["githubContributors"] = True
+                else:
+                    graph.nodes[node]["searchData"] = {"githubContributors": True}
+
+            else:
+                # We have already added the contributors
+                pass
+
+    return graph
