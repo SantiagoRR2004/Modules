923f77f3f2aa5c93e602facc8d187d997a11b0f2
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-12-24 10:46:32 +0100

    Added removeUnconnectedNodes function

--- Modules/graph.py
+++ Modules/graph.py
@@ -1,6 +1,7 @@
 from pyvis.network import Network
 from PIL import ImageColor
 import networkx as nx
+import copy
 
 
 def createHTML(graph: nx.MultiDiGraph) -> str:
@@ -90,5 +91,114 @@ def removeDuplicateEdges(graph: nx.MultiDiGraph) -> nx.MultiDiGraph:
         if eTuple not in seen:
             seen.add(eTuple)
             cleanedGraph.add_edge(u, v, **attributes)
+
+    return cleanedGraph
+
+
+def removeUnconnectedNodes(
+    graph: nx.MultiDiGraph, desiredType: str = "Known"
+) -> nx.MultiDiGraph:
+    """
+    Removes the nodes that do are not part
+    of a path bewteen two nodes of the desiredType.
+
+    Args:
+        - graph (nx.MultiDiGraph): The graph to be cleaned.
+        - desiredType (str): The desiredType of the nodes to be kept.
+
+    Returns:
+        nx.MultiDiGraph: The cleaned graph.
+    """
+    cleanedGraph = copy.deepcopy(graph)
+
+    """
+    We start by iteratively eliminating all the nodes
+    that only have one connection and are not of the desiredType.
+    """
+
+    continueFlag = True
+    nNodes = len(cleanedGraph.nodes())
+
+    while continueFlag:
+        cleanedGraph.remove_nodes_from(
+            [
+                node
+                for node in cleanedGraph
+                if len(
+                    set(n for n in cleanedGraph.successors(node)).union(
+                        set(n for n in cleanedGraph.predecessors(node))
+                    )
+                )
+                < 2
+                and desiredType not in cleanedGraph.nodes[node]["type"]
+            ]
+        )
+
+        if nNodes == len(cleanedGraph.nodes()):
+            continueFlag = False
+
+        nNodes = len(cleanedGraph.nodes())
+
+    """
+    Now we need to eliminate all the nodes that
+    can't reach directly 2 nodes of the desiredType.
+    """
+
+    goodNodes = [
+        node for node in cleanedGraph if desiredType in cleanedGraph.nodes[node]["type"]
+    ]
+
+    notCheckedSymbol = "Not Checked"
+
+    eliminateNodes = {
+        node: notCheckedSymbol for node in cleanedGraph if node not in goodNodes
+    }
+
+    for n in list(eliminateNodes.keys()):
+        if eliminateNodes[n] == notCheckedSymbol:
+            goodFoundNodes = set()
+            foundNodes = set([n])
+            frontier = [n]
+
+            while frontier:
+                node = frontier.pop()
+
+                for neighbor in set(cleanedGraph.successors(node)) | set(
+                    cleanedGraph.predecessors(node)
+                ):
+
+                    if neighbor in goodNodes:
+                        # It means there is a path to a desiredType node
+                        goodFoundNodes.add(neighbor)
+
+                    elif neighbor not in foundNodes:
+                        foundNodes.add(neighbor)
+                        frontier.append(neighbor)
+
+                        if (
+                            not eliminateNodes[neighbor]
+                            and eliminateNodes[neighbor] != notCheckedSymbol
+                        ):
+                            # The neighbour has been marked as safe
+                            # We fill the goodFoundNodes to mark all the foundNodes as not to be eliminated
+                            # 2 and 3 are just random numbers
+                            goodFoundNodes.add("Garbage 1")
+                            goodFoundNodes.add("Garbage 2")
+
+                if len(goodFoundNodes) >= 2:
+                    for node in foundNodes:
+                        eliminateNodes[node] = False
+                    frontier = []
+
+            if len(goodFoundNodes) < 2:
+                for node in foundNodes:
+                    eliminateNodes[node] = True
+
+    # We check that all the values are booleans
+    assert all(isinstance(eliminateNodes[node], bool) for node in eliminateNodes)
+
+    cleanedGraph.remove_nodes_from(
+        [node for node in eliminateNodes if eliminateNodes[node]]
+    )
 
     return cleanedGraph
